3b9e5d54b64eca0c0ab8dda22412feda213660c5
Jihaysse <js.segers@hotmail.com>
2022-02-17 00:57:28 +0100

    Added delete student feature

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
 import { GenderService } from 'src/app/services/gender.service';
@@ -41,7 +41,8 @@ export class ViewStudentComponent implements OnInit {
     private readonly studentService: StudentService,
     private readonly route: ActivatedRoute,
     private readonly genderService: GenderService,
-    private snackbar: MatSnackBar) { }
+    private snackbar: MatSnackBar,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
@@ -71,16 +72,40 @@ export class ViewStudentComponent implements OnInit {
     // Call student service to update the student
     this.studentService.updateStudent(this.student.id, this.student)
     .subscribe(
-      (successResponse) => {
-        // Show notification
-        this.snackbar.open('Student updated successfully', undefined, {
-          duration: 3000
-        });
-      },
-      (error) => {
-        console.log(error);
+      {
+        next: () => {
+          // Show notification
+          this.snackbar.open('Student updated successfully', undefined, {
+            duration: 3000
+          });
+        },
+        error: (error) => {
+          console.log(error);
+        }
       });
-
   }
+
+  onDelete(): void {
+    // Call student service to delete the student
+    this.studentService.deleteStudent(this.student.id)
+    .subscribe(
+      {
+      next: (successResponse) => {
+        // Notification
+        this.snackbar.open('Student deleted successfully', undefined, {
+          duration: 2000
+        });
+
+        // Wait until notification is gone
+        setTimeout(() => {
+          // Go back to students table
+          this.router.navigateByUrl('students');
+        }, 2000);
+      },
+      error: (errorResponse) => {
+        console.log(errorResponse);
+      }
+    })
+  };
 
 }
